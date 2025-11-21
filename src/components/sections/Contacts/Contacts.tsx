77be6d1a8ed794bed30ba1f48eeecd084b23d9ed
cla91 <83760330+cla91@users.noreply.github.com>
2025-11-21 19:57:35 +0100

    feat: add Contacts section with styles and content for contact information

---
+++ src/components/sections/Contacts/Contacts.tsx
@@ -0,0 +1,29 @@
+import styles from "@/components/sections/Contacts/Contacts.module.scss";
+
+export default function Contacts() {
+  return (
+    <section id="contatti" className={`container navigableSection`}>
+      <h2>Contatti</h2>
+      <p className={styles.intro}>
+        Per collaborazioni o domande, puoi contattarmi tramite questi canali.
+      </p>
+
+      <ul className={styles.contactsList}>
+        <li>
+          <h3>Email</h3>
+          <a href="mailto:cclaudia0691@gmail.com">cclaudia0691@gmail.com</a>
+        </li>
+        <li>
+          <h3>GitHub</h3>
+          <a href="https://github.com/cla91">github.com/cla91</a>
+        </li>
+        <li>
+          <h3>LinkedIn</h3>
+          <a href="https://linkedin.com/in/claudia-cantiani">
+            linkedin.com/in/claudia-cantiani
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
